f458d2196aede08a72b143351f110b252a53b74e
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-04-17 22:42:22 -0600

    Create run_wordpress.sh

---
+++ run_wordpress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#create directories if necessary
+if ! [ -d /tmp/run/httpd ]; then mkdir -p /tmp/run/httpd;fi
+if ! [ -d /tmp/log/httpd ]; then mkdir -p /tmp/log/httpd;fi
+if ! [ -d /tmp/lock/httpd ]; then mkdir -p /tmp/lock/httpd;fi
+
+
+# install wordpress if not in persistentvolume
+if [ ! -f /var/www/html/index.php ]; then
+  cp -rf /moodle/* /var/www/html/
+  
+fi
+
+echo "openshift-wordpress:x:`id -u`:0:openshift-wordpress:/:/sbin/nologin" >> /etc/passwd
+
+#run Apache
+httpd -f /httpd.conf -D FOREGROUND
