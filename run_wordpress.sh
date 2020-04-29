1a81e286c4539193d7433c3b04d4ae8477410afd
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-04-29 11:28:03 -0600

    Update run_wordpress.sh

--- run_wordpress.sh
+++ run_wordpress.sh
@@ -57,7 +57,7 @@ process_extending_files ${APP_DATA}/php-pre-start/ ${PHP_CONTAINER_SCRIPTS_PATH}
 
 #############################################################################################################
 # install wordpress if not in persistentvolume
-if [ ! -f /var/www/html/index.php ]; then
+if [ ! -f /opt/app-root/src/index.php ]; then
   cp /index.html /opt/app-root/src
 fi
 echo "openshift-wordpress:x:`id -u`:0:openshift-wordpress:/:/sbin/nologin" >> /etc/passwd
