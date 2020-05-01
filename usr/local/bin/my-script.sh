a047a9b4c8eca87e0f5dfbfc68dbd5c43a481b31
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-05-01 11:28:41 -0600

    Delete my-script.sh

--- usr/local/bin/my-script.sh
+++
@@ -1,4 +0,0 @@
-* * * * * /usr/bin/php  /path/to/moodle/admin/cli/cron.php >/dev/null
-* * * * * /usr/bin/php  /path/to/moodle/admin/cli/cron.php
-* * * * * php  /path/to/moodle/admin/cli/cron.php >/dev/null
-* * * * * root php /path/to/moodle/admin/cli/cron.php
