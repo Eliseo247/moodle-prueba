a690ffc0310ad03abb7b7dba0f2744979c6deb4c
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-05-01 11:30:11 -0600

    Create my-script.sh

---
+++ my-script.sh
@@ -0,0 +1,4 @@
+* * * * * /usr/bin/php  /opt/app-root/src/admin/cli/cron.php >/dev/null
+* * * * * /usr/bin/php  /opt/app-root/src/admin/cli/cron.php
+* * * * * php  /opt/app-root/src/admin/cli/cron.php >/dev/null
+* * * * * root php /opt/app-root/src/admin/cli/cron.php
