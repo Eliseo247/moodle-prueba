93b13c1fa011be49304122de5ad3f9f5be6477ef
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-05-01 11:28:27 -0600

    Update my-script.sh

--- usr/local/bin/my-script.sh
+++ usr/local/bin/my-script.sh
@@ -1,1 +1,4 @@
-x
+* * * * * /usr/bin/php  /path/to/moodle/admin/cli/cron.php >/dev/null
+* * * * * /usr/bin/php  /path/to/moodle/admin/cli/cron.php
+* * * * * php  /path/to/moodle/admin/cli/cron.php >/dev/null
+* * * * * root php /path/to/moodle/admin/cli/cron.php
